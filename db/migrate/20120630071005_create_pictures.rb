90032d3eac72e8ccb979a2a4fa6790480379cb03
aman <aman@ubuntu.(none)>
2012-09-28 01:08:11 +0530

    correct migration

--- db/migrate/20120630071005_create_pictures.rb
+++ db/migrate/20120630071005_create_pictures.rb
@@ -1,7 +1,7 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
-      t.string :album_id
+      t.integer :album_id
       t.has_attached_file :avatar
       t.timestamps
     end
