660c2f6fd4e262f0f7142e5cf9165bf815536e0b
aman <aman@ubuntu.(none)>
2012-10-05 00:15:07 +0530

    reset database

--- db/migrate/20120630055133_create_users.rb
+++ db/migrate/20120630055133_create_users.rb
@@ -1,11 +1,17 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string    :name
       t.string    :email
       t.string    :crypted_password
       t.string    :password_salt
       t.string    :persistence_token
+      t.string    :provider
+      t.string    :uid
+      t.string    :oauth_token
+      t.datetime  :oauth_expires_at
+      t.string    :first_name
+      t.string    :last_name
+      t.string    :identifier_url
       t.timestamps
     end
   end
